703f40b4842d117b9fd5267455350c3d9cb41d50
Enow-moh <116763125+enowmoh@users.noreply.github.com>
2023-01-18 10:52:33 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+module "tomcat" {
+source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+  name = var.name_input
 
+  ami                    = var.my_ami
+  instance_type          = "t2.medium"
+  key_name               = var.key
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
